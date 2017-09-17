7446deee6943237ab873af2bb4d4d94c76ba4fb4
こぴなたみぽ <syuilotan@yahoo.co.jp>
2017-09-18 07:48:17 +0900

    Fix typo

--- src/types/any.ts
+++ src/types/any.ts
@@ -3,7 +3,7 @@ import Query from '../query';
 /**
  * Any
  */
-export default class BooleanQuery extends Query<boolean> {
+export default class AnyQuery extends Query<boolean> {
 	constructor(optional: boolean, nullable: boolean, lazy: boolean, value?: any) {
 		super(optional, nullable, lazy, value);
 	}
