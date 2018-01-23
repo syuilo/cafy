b4200dddab83436ad711e77cabe7cc90b2b633d7
syuilo <syuilotan@yahoo.co.jp>
2018-01-23 11:44:26 +0900

    :v:

--- src/types/any.ts
+++ src/types/any.ts
@@ -3,7 +3,7 @@ import Query from '../query';
 /**
  * Any
  */
-export default class AnyQuery extends Query<boolean> {
+export default class AnyQuery extends Query<any> {
 	constructor(optional: boolean, nullable: boolean, lazy: boolean, value?: any) {
 		super(optional, nullable, lazy, value);
 	}
